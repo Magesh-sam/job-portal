07a2662406c5ae98bc4541d3af6eda9b8300d6a5
mageshkannan <mageshkannanam@gmail.com>
2025-02-10 11:08:32 +0530

    Update Unauthorized component text color for improved visibility

--- src/components/Unauthorized.tsx
+++ src/components/Unauthorized.tsx
@@ -5,7 +5,7 @@ const Unauthorized = () => {
   return (
     <main className="min-h-screen bg-background container mx-auto px-4 py-8 flex flex-col gap-3 items-center justify-center">
       <h1 className="text-4xl font-bold text-red-500">Unauthorized ! </h1>
-      <p className="text-gray-600 mt-2">
+      <p className="text-gray-400 mt-2">
         Please login as different user to view this page
       </p>
       <Button onClick={() => navigate("/")} className="mt-3">
