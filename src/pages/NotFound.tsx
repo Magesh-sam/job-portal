504febeb6a2fdca977a17d170297c3606cee492d
mageshkannan <mageshkannanam@gmail.com>
2025-02-17 13:33:10 +0530

    Enhance NotFound page with navigation buttons for better user experience

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,16 +1,19 @@
 import { Button } from "@/components/ui/button";
-import { House } from "lucide-react";
-import { Link } from "react-router";
+import { ChevronLeft, House } from "lucide-react";
+import { Link, useNavigate } from "react-router";
 function NotFound() {
+  const navigate = useNavigate();
   return (
     <main className="flex flex-col items-center justify-center h-screen gap-5">
+      <Button aria-label="Go back home" onClick={() => navigate(-1)}>
+        Go back
+        <ChevronLeft />
+      </Button>
+      <Button aria-label="Go back home" onClick={() => navigate("/")}>
+        Go back to home page
+        <House />
+      </Button>
       <h1>404: The page you are looking for was Not Found</h1>
-      <Link to="/">
-        <Button aria-label="Go back home">
-          Go back home
-          <House />
-        </Button>
-      </Link>
 
       <img
         src="/404.svg"
